4555fb80a97cc4f2bb378ead51c73905f2163935
Lyumih <32732179+Lyumih@users.noreply.github.com>
2020-10-02 08:56:30 +0300

    feat(project): client auth

--- layout/AuthorizedLayout.tsx
+++ layout/AuthorizedLayout.tsx
@@ -1,16 +1,36 @@
 import { Layout, Menu } from "antd";
-import { FC } from "react";
+import { useRouter } from "next/dist/client/router";
+import { FC, useEffect, useState } from "react";
 import AuthorizedMenu from "../components/Menu/AuthorizedMenu";
 
 interface AuthorizedLayoutProps {}
 
 const AuthorizedLayout: FC<AuthorizedLayoutProps> = ({ children }) => {
+  const router = useRouter();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (token) {
+      console.log(token);
+    } else {
+      let tokenClient = localStorage.getItem("token");
+
+      if (tokenClient) {
+        setToken(tokenClient);
+      } else {
+        window.location.href = "/login";
+      }
+      // router.push("/login");
+    }
+  }, [token]);
   return (
     <Layout>
       <Layout.Header>
         <AuthorizedMenu />
       </Layout.Header>
-      <Layout.Content style={{ padding: "2rem" }}>{children}</Layout.Content>
+      <Layout.Content style={{ padding: "2rem" }}>
+        {token && children}
+      </Layout.Content>
       {/* <Layout.Footer>
         <h2>Footer</h2>
       </Layout.Footer> */}
